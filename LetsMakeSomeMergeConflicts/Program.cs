8caa7c47dad51f5d5e74b1244992704cb8f5845b
Swoopsy <38360122+Swoopsy@users.noreply.github.com>
2020-08-07 09:53:21 -0400

    lets make a merge conflict

--- LetsMakeSomeMergeConflicts/Program.cs
+++ LetsMakeSomeMergeConflicts/Program.cs
@@ -6,7 +6,7 @@ namespace LetsMakeSomeMergeConflicts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("The quick brown fox jumped over the lazy dog");
         }
     }
 }
