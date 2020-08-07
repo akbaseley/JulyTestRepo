6cc5db92fa04501ad5b9d4518eee824715d86543
Anna Baseley <akbaseley@gmail.com>
2020-08-07 09:59:14 -0400

    MERGE CONFLICT TIME

--- LetsMakeSomeMergeConflicts/Program.cs
+++ LetsMakeSomeMergeConflicts/Program.cs
@@ -6,7 +6,7 @@ namespace LetsMakeSomeMergeConflicts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("C# is the greatest!");
         }
     }
 }
